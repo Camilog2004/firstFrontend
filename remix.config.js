38ba756ba9a79bd967eaf22a826d716f45102063
Camilog2004 <juagutierrezma@unal.edu.co>
2025-06-26 09:10:34 -0500

    deleting amplify folder

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+    future: {
+      v2_routeConvention: true
+    },
+    appDirectory: "app",
+    ignoredRouteFiles: ["**/.*"],
+    output: "static",
+};
+  
